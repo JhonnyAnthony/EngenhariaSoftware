0155d3cc43ed325a94d5640fa490b58a77d1a940
JHONNY ANTHONY DE SOUZA <jhonny.souza@catolicasc.edu.br>
2026-05-15 20:38:34 -0300

    f

---
+++ CodigoC/Laço de Repetição/atividades/contagem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int count = 0;
+    while (count < 5)
+    {
+        printf("Contagem: %d\n", count);
+        count++;
+    }
+    
+    return 0;
+
+}
