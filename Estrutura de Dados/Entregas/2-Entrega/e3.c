549521371dd927dbfab9ee4ba38943643312e791
Jhonny Anthony <jhonnysouza@JhonnyAnthony.localdomain>
2026-09-01 19:41:36 -0300

    feat: two exercicies to AULA 05 - Lista de atividades

--- Estrutura de Dados/Entregas/2-Entrega/e3.c
+++ Estrutura de Dados/Entregas/2-Entrega/e3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+void primeiraMaiuscula(char *nome);
 int main(){
     // Implemente um programa em C ANSI que leia um nome completo
     // armazenado em uma string. O programa deve criar a função 
@@ -10,5 +12,20 @@ int main(){
     // espaço. As demais letras de cada palavra não devem ser modificadas. 
     // Por exemplo, se o usuário digitar: manfred heil junior o programa
     // deverá retornar: Manfred Heil Junior
+    char nome[100];
+    printf("Digite um nome completo\n");
+    fgets(nome, 100, stdin);
+    primeiraMaiuscula(nome);
+    printf("Nome com primeira letra de cada palavra em maiúscula: %s", nome);
+    return 0;
+}
 
+void primeiraMaiuscula(char *nome){
+    int i =0;
+    if (nome[i] >= 'a' && nome[i] <= 'z') {
+        nome[i] = nome[i] - 32; 
+    }
+    else{
+        printf("O primeiro caractere não é uma letra minúscula.\n");
+    }
 }
