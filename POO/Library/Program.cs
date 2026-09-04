e93f7da7bd6ce8c4539bbbc5e7bba32f071dba32
jhonny.souza <jhonnyanthony47@gmail.com>
2026-09-04 19:54:35 -0300

    feat: Architeture - Create 10 new classes
    Studant MVC, Book MVC and Screen

--- POO/Library/Program.cs
+++ POO/Library/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace Library
     {
         static void Main(string[] args)
         {
+            //MVC - Model , View , Controller
+            //Model - Dados
+            //View - Tela
+            //Controller - Regras e Sequencia de Tarefas
+
+            //EXEMPLOS:
+            //Cada funcionalidade do sistema terá uma **entidade de negocio** Model, View e Controller
+            //LivroModel, LivroView, LivroController
+            //AlunoModel, AlunoView, AlunoController
+            //Classes Auxiliares
+            //Tela
+
         }
     }
 }
