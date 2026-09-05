5125ce0cf36090d40d90b27e2995609d23d150d5
jhonny.souza <jhonnyanthony47@gmail.com>
2026-09-04 22:14:27 -0300

    feat: create view to show the CRUD front-end

--- POO/Library/Book/BookView.cs
+++ POO/Library/Book/BookView.cs
@@ -8,5 +8,16 @@ namespace Library
 {
     internal class BookView
     {
+        //atributos
+
+        //construtor
+
+        //métodos
+        public void BuildScreen(int col, int lin, string title)
+        {
+            Screen screenBook = new Screen();
+            screenBook.BuildScreen(col, lin, col + 30, lin + 8, "Create Book");
+            Console.Read(); // TESTE - para ver a tela antes de fechar
+        }
     }
 }
