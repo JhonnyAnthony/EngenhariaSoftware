30472cffb68fe00060005377f96e0310a68f1691
jhonny.souza <jhonnyanthony47@gmail.com>
2026-09-04 22:16:31 -0300

    feat: orquestration and create interaction to system

--- POO/Library/Program.cs
+++ POO/Library/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Management.Instrumentation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library
 {
@@ -22,7 +18,33 @@ namespace Library
             //AlunoModel, AlunoView, AlunoController
             //Classes Auxiliares
             //Tela
+            Screen myScreen = new Screen(ConsoleColor.Magenta, ConsoleColor.Gray);
+            myScreen.ConfigureScreen();
+            BookController bookController = new BookController();
 
+            int op;
+
+            List<string> optionsMenu = new List<string>();
+            optionsMenu.Add("1  -  Books      ");
+            optionsMenu.Add("2  -  Studants   ");
+            optionsMenu.Add("3  -  Loans      ");
+            optionsMenu.Add("4  -  Relatórios ");
+            optionsMenu.Add("0  -  Sair       ");
+
+            while (true)
+            {
+                myScreen.BuildScreen(0, 0, 79, 24, "Library Sistem");
+                op = int.Parse(myScreen.ShowMenu(optionsMenu, 2, 2));
+
+                if (op == 0)
+                {
+                    Console.WriteLine("Saindo do sistema...");
+                    break;
+                }
+                else if (op == 1) { 
+                    bookController.RunCRUD();
+                }
+            }
         }
     }
 }
