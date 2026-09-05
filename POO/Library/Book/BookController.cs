9fbfa071add4decc9f8d365985d5a56215cb153b
jhonny.souza <jhonnyanthony47@gmail.com>
2026-09-04 22:13:35 -0300

    feat: Create a controller to create a CRUD to books class

--- POO/Library/Book/BookController.cs
+++ POO/Library/Book/BookController.cs
@@ -8,5 +8,25 @@ namespace Library
 {
     internal class BookController
     {
+        // atributos
+        // DECLARAÇÃO DE ATRIBUTOS
+        private BookModel _bookModel;
+        private BookView _bookView;
+        private List<BookModel> _bookModels;
+
+        // construtor
+        public BookController()
+        {
+            // INSTANCIAÇÃO DE ATRIBUTOS
+            this._bookModel     = new BookModel();
+            this._bookView      = new BookView();
+            this._bookModels    = new List<BookModel>();
+        }
+
+        // métodos
+        // orquestrador do CRUD
+        public void RunCRUD() {
+            this._bookView.BuildScreen(10, 5, "Cadastro");
+        }
     }
 }
